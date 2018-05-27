27e6f630728afad6bfff72957bad2435d786c9eb
zonoskar <zonoskar@gmail.com>
2018-05-27 12:13:01 +0200

    CSV logging now creates the directory when needed

--- csvoutput.py
+++ csvoutput.py
@@ -1,16 +1,27 @@
 import time
+import os
+
   
 class csvoutput :
 
    #--------------------------------------------------------------------------
    def __init__( self, dirname, filename):
-   # This class provides an interface to a CSV fiel output of the measured
+   # This class provides an interface to a CSV file output of the measured
    # Goodwe inverter data
    #
       if dirname[:-1] != '/':
          dirname += '/'
       if filename[:-1] != '_':
          filename += '_'
+      
+      self.csv_logging_enabled = True
+      
+      if not (os.path.exists( dirname) and os.path.isdir( dirname)):
+         try:
+	    os.makedirs(dirname)
+	 except Exception, ex:
+	    print "CSV logging has been disabled because dir %s could not be created (%s)" % (dirname, ex)
+	    self.csv_logging_enabled = False
       self.m_filename = dirname + filename
       self.m_write_header = True
 
@@ -20,19 +31,20 @@ class csvoutput :
    # Writes the Goodwe inverted data to a CSV file. The filename is based on
    # the current date and time. When a header is not written, this is done.
    #
-      t = time.localtime()
-      dateString = str(t.tm_year).zfill(4) + str(t.tm_mon).zfill(2) + str(t.tm_mday).zfill(2) + ',' + str(t.tm_hour).zfill(2) + ':' + str(t.tm_min).zfill(2) + ','
-      try:
-         openname = self.m_filename + str(t.tm_year).zfill(4) + str(t.tm_mon).zfill(2) + str(t.tm_mday).zfill(2) + ".csv"
+      if self.csv_logging_enabled:
+         t = time.localtime()
+         dateString = str(t.tm_year).zfill(4) + str(t.tm_mon).zfill(2) + str(t.tm_mday).zfill(2) + ',' + str(t.tm_hour).zfill(2) + ':' + str(t.tm_min).zfill(2) + ','
+         try:
+            openname = self.m_filename + str(t.tm_year).zfill(4) + str(t.tm_mon).zfill(2) + str(t.tm_mday).zfill(2) + ".csv"
 
-         with open(openname, "a") as fs:
-            if self.m_write_header:
-               fs.write( pvout.get_csv_header() + '\n')
-               # next time, no need for a header
-	       self.m_write_header = False
-            fs.write( dateString + pvout.to_csv_string() + '\n')
-      except Exception, arg:
-         print "Write CSV data Error: " + str(arg)
+            with open(openname, "a") as fs:
+               if self.m_write_header:
+                  fs.write( pvout.get_csv_header() + '\n')
+                  # next time, no need for a header
+	          self.m_write_header = False
+               fs.write( dateString + pvout.to_csv_string() + '\n')
+         except Exception, arg:
+            print "Write CSV data Error: " + str(arg)
      
 
    #--------------------------------------------------------------------------
